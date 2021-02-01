71d32486c12acc04b2bc17f43506b4b8e45d415f
vroni-g <veronika.grupp@posteo.de>
2021-02-01 14:43:13 +0100

    added change point detection methods

--- testing/CPD_tests.R
+++ testing/CPD_tests.R
@@ -1,7 +1,7 @@
 # simulation to check FPR of change point detection methods
 
 trend <- rep(sin(2*pi*c(0:11)/12),20) #+c(rep(0,12*10),rep(0.5,12*10))
-n <- 5000
+n <- 10000
 
 # BFAST ----
 #*********************************************************************
@@ -12,10 +12,9 @@ for (i in 1:n) {
   res <- bfast(d, max.iter=10)
   if (!is.na(res$Time)) ti <- c(ti, res$Time)
 }
-length(ti)
-if (length(ti) > 5) hist(ti, col="lightblue", br=length(ti)/2)
-length(ti)/n # should be 0.05
-plot(d) # plot the last t.s. as an example
+#length(ti)
+#if (length(ti) > 5) hist(ti, col="lightblue", br=length(ti)/2)
+length(ti)/n # is 0.0052
 
 
 # strucchange package ----
@@ -37,41 +36,59 @@ test_CPD_efp <- function(n, fun, trend, h = NULL){
   return(p)
 }
 
-n <- 5000
+n <- 10000
 # residual based:
 rec_cusum <- test_CPD_efp(n, 'Rec-CUSUM', trend)
 ols_cusum <- test_CPD_efp(n, 'OLS-CUSUM', trend)
-rec_mosum <- test_CPD_efp(n, 'Rec-MOSUM', trend, h = 0.3) # tried different window sizes from 0.01 to 0.3 but always FPR = 0
+score_cusum <- test_CPD_efp(n, 'Score-CUSUM', trend)
+rec_mosum <- test_CPD_efp(n, 'Rec-MOSUM', trend, h = 0.01) # tried different window sizes from 0.01 to 0.3 but always FPR = 0
 ols_mosum <- test_CPD_efp(n, 'OLS-MOSUM', trend, h = 0.01)
 # estimates based:
 recur_estimates <- test_CPD_efp(n, 'RE', trend)
 mov_estimates <- test_CPD_efp(n, 'ME', trend, h = 0.05)
 
-# F Statistics (compares model fit for whole series vs. two segments; iterates over whole series as possible break points)
-#************
-test_CPD_fstat <- function(n){
-  p_sup <- c()
-  p_exp <- c()
-  p_ave <- c()
-  for (i in 1:n) {
-    d <- ts(rnorm(12*20,0,.2)+trend, frequency=12, start=c(2000,1))
-    temp <- Fstats(d ~ 1)
-    res_sup <- sctest(temp, type = 'supF')
-    p_sup <- c(p_sup, res_sup$p.value)
-    res_exp <- sctest(temp, type = 'expF')
-    p_exp <- c(p_exp, res_exp$p.value)
-    res_ave <- sctest(temp, type = 'aveF')
-    p_ave <- c(p_ave, res_ave$p.value)
-  }
-  fpr_sup <- sum(p_sup<0.05)/n
-  cat(paste('False positive rate for supF is: ', fpr_sup))
-  fpr_exp <- sum(p_exp<0.05)/n
-  cat(paste('False positive rate for expF is: ', fpr_exp))
-  fpr_ave <- sum(p_ave<0.05)/n
-  cat(paste('False positive rate for aveF is: ', fpr_ave))
-}
+# all result in FPR = 0...
 
-fstat <- test_CPD_fstat(10000)
+# Generalized M-Fluctuation Tests
+#************
+p_LM <- c()
+p_maxmo <- c()
+n <- 10000
+for (i in 1:n) {
+  d <- ts(rnorm(12*20,0,.2)+trend, frequency=12, start=c(2000,1))
+  temp <- gefp(d ~ 1, fit = lm)
+  res_LM <- sctest(temp, functional = supLM(0.05))
+  p_LM <- c(p_LM, res_LM$p.value)
+  res_maxmo <- sctest(temp, functional = maxMOSUM(width = 0.05))
+  p_maxmo <- c(p_maxmo, res_maxmo$p.value)
+}
+fpr_LM <- sum(p_LM<0.05)/n # is 0
+cat(paste('False positive rate for supLM is: ', fpr_LM))
+fpr_maxmo <- sum(p_maxmo<0.05)/n # is 0
+cat(paste('False positive rate for maximum MOSUM is: ', fpr_maxmo))
+
+
+# F Statistics (compares model fits for whole series vs. two segments; iterates over whole series as possible break points)
+#************
+p_sup <- c()
+p_exp <- c()
+p_ave <- c()
+for (i in 1:n) {
+  d <- ts(rnorm(12*20,0,.2)+trend, frequency=12, start=c(2000,1))
+  temp <- Fstats(d ~ 1)
+  res_sup <- sctest(temp, type = 'supF')
+  p_sup <- c(p_sup, res_sup$p.value)
+  res_exp <- sctest(temp, type = 'expF')
+  p_exp <- c(p_exp, res_exp$p.value)
+  res_ave <- sctest(temp, type = 'aveF')
+  p_ave <- c(p_ave, res_ave$p.value)
+}
+fpr_sup <- sum(p_sup<0.05)/n # is 0
+cat(paste('False positive rate for supF is: ', fpr_sup))
+fpr_exp <- sum(p_exp<0.05)/n # is 0
+cat(paste('False positive rate for expF is: ', fpr_exp))
+fpr_ave <- sum(p_ave<0.05)/n # is 0
+cat(paste('False positive rate for aveF is: ', fpr_ave))
 
 
 # AMOC (changepoint package) ----
@@ -86,11 +103,11 @@ for (i in 1:n) {
   d <- ts(rnorm(12*20,0,.2)+trend, frequency=12, start=c(2000,1))
   res <- cpt.mean(d, penalty = "Asymptotic", pen.value = 0.05, method = "AMOC",
                      test.stat = "Normal", class = FALSE)
-  # in documentation says that it results in NA if no changepoint but it always
-  # returns 240 as changepoint location which is the last point...
+  # in documentation says that it results in NA if no changepoint but it often
+  # returns 240 as changepoint location which is the last point, so only include results unlike 240
   if (res[1] != 240) fpr_mean <- c(fpr_mean, res[1])
 }
-cat(paste('False positive rate for AMOC mean is ', length(fpr_mean)/n))
+cat(paste('False positive rate for AMOC mean is ', length(fpr_mean)/n)) # fpr is 0
 
 fpr_var <- c()
 for (i in 1:n) {
@@ -99,8 +116,7 @@ for (i in 1:n) {
                   test.stat = "Normal", class = FALSE)
   if (res[1] != 240) fpr_var <- c(fpr_var, res[1])
 }
-cat(paste('False positive rate for AMOC var is ', length(fpr_var)/n))
-# detects a handful of change points but only within the first and last two or three time points
+cat(paste('False positive rate for AMOC var is ', length(fpr_var)/n)) # fpr is 0.001
 
 fpr_meanvar <- c()
 for (i in 1:n) {
@@ -109,18 +125,16 @@ for (i in 1:n) {
                   test.stat = "Normal", class = FALSE)
   if (res[1] != 240) fpr_meanvar <- c(fpr_meanvar, res[1])
 }
-cat(paste('False positive rate for AMOC meanvar is ', length(fpr_meanvar)/n))
+cat(paste('False positive rate for AMOC meanvar is ', length(fpr_meanvar)/n)) # fpr is 1
 fpr_meanvar
-# always detects change points at the penultimate and second/third time points...
+# always detects change points at the penultimate and/or second/third time points...
 
 
 # Pettitts (trend package) ----
 #*********************************************************************
 # non parametric pettitt test (also somehow based on wilcoxon rang thing)
-
 library(trend)
 #trend <- rep(sin(2*pi*c(0:11)/12),40) # larger time series (adjust the ts() command!)
-
 n <- 10000
 p <- c()
 for (i in 1:n) {
@@ -129,5 +143,54 @@ for (i in 1:n) {
   p <- c(p, res$p.value)
 }
 fpr <- sum(p<0.05)/n
-cat(paste('False positive rate for pettitts test is: ', fpr))
+cat(paste('False positive rate for pettitts test is: ', fpr)) # is 0
 
+# cpm package ----
+#*********************************************************************
+library(cpm)
+
+test_CPD_cpm <- function(n, fun, trend){
+  bp <- 0
+  for (i in 1:n) {
+    d <- ts(rnorm(12*20,0,.2)+trend, frequency=12, start=c(2000,1))
+    res <- detectChangePointBatch(d, cpmType = fun, alpha = 0.05)
+    if (res$changeDetected) bp <- c(bp, res$changePoint)
+  }
+  fpr <- length(bp)/n
+  cat(paste('False positive rate for cpm function ', fun,' is: ', fpr))
+  return(bp)
+}
+
+n <- 10000
+stud <- test_CPD_cpm(n, 'Student', trend) # 0.0008
+bart <- test_CPD_cpm(n, 'Bartlett', trend) # 0.03
+glr <- test_CPD_cpm(n, 'GLR', trend) # 0.13
+mw <- test_CPD_cpm(n, 'Mann-Whitney', trend) # 0.003
+mood <- test_CPD_cpm(n, 'Mood', trend) # 0.001
+lep <- test_CPD_cpm(n, 'Lepage', trend) # 0.0227
+ks <- test_CPD_cpm(n, 'Kolmogorov-Smirnov', trend) # 0.0002
+cvm <- test_CPD_cpm(n, 'Cramer-von-Mises', trend) # 0.0117
+
+# segmented package ----
+#*********************************************************************
+library(segmented)
+library(TSstudio)
+library(tidyverse)
+
+# not yet working finish this...
+
+# fit <-lm(d~1)
+# pscore.test(fit)
+#
+# d <- ts(rnorm(12*20,0,.2)+trend, frequency=12, start=c(2000, 1))
+# df <- ts_reshape(d, type='long')
+# df <- df %>% mutate(.,
+#                     time = paste(month, '-', year),
+#                     .keep='unused')
+#
+# seg <- segmented(lm(value ~ 1, data = df), ~ time)
+#
+# Nile_df <- data.frame(Nile = as.numeric(Nile), Year = 1871:1970)
+# seg_nile <- segmented(lm(Nile ~ 1, data = Nile_df), ~ Year,
+#                       psi = list(Year = c(1900))) # using 1900 as starting value
+# summary(seg_nile)
