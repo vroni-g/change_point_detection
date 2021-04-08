dd639a7c1553701397d65e92a65832d7ead317af
Jose Cortes <20524582+jcortesr@users.noreply.github.com>
2021-04-08 17:52:10 +0200

    2d approach

---
+++ R/array_to_matrix.R
@@ -0,0 +1,53 @@
+# Make an array a 2d matrix
+
+#' Input is a data array where the first two dimensions are in space(x,y;lat,lon)
+#' and the third dimension is time.
+#'
+#' Outputs a list, with the non-zero pixels being a matrix (columns = pixels,
+#' rows=time steps), s
+#'
+#' @param data Data in a 3d array, where the first two dimensions are the
+#' physical dimensions (e.g., lon and lat), and the third one is time.
+#'
+#' @return returns the distribution of maxT, stcs, and stcs_mvt, each a vector in a list
+#' @export perm_dist
+#'
+array_to_matrix<- function(data){
+  nrows<- dim(data)[1]
+  ncols<- dim(data)[2]
+  time_points<- dim(data)[3]
+
+  out<- vector("list", length = time_points)
+  for (i in 1:time_points){
+    out[[i]]<- data[,,i]
+  }
+  names(out)<- as.character(1:time_points)
+
+  sel<- as.numeric(!is.na(as.vector(out[[1]])))
+
+  if(length(out) > 1){ # when imputing image time series it selects only grid cells with < 10% missing data
+
+    for(i in 2:time_points){
+      sel <- as.numeric(!is.na(as.vector(out[[i]]))) + sel
+      print(summary(sel))
+    }
+    sel<- sel > (time_points*.9) # keep only data points with 90% or more data
+  }
+
+  wh.sel<- which(as.vector(sel), arr.ind = TRUE)
+  V = sum(sel)
+  Y = matrix(NA, nrow=time_points, ncol=V)
+  for (time_point in 1:time_points) {
+    Y[ time_point , ] = as.vector(out[[time_point]])[sel]
+  }
+
+  # also elimiate if scores are constant (e.g., all 0)
+  constant<- apply(Y, 2, function(x) diff(range(x, na.rm = TRUE)) < .Machine$double.eps ^ 0.5)
+  if(sum(constant, na.rm = TRUE)>0){
+    wh_constant<- which(constant)
+    Y<- Y[,-wh_constant]
+    sel[wh.sel][wh_constant]<- FALSE
+    wh.sel<- which(as.vector(sel), arr.ind = TRUE)
+  }
+  return(list(Y=Y, sel = sel, wh.sel = wh.sel, nrow = nrows, ncol = ncols))
+}
--- testing/tippet_hpc_lai.R
+++ testing/tippet_hpc_lai.R
@@ -4,7 +4,6 @@ suppressMessages(library(devtools))
 suppressMessages(library(tidyverse))
 devtools::load_all("/home/veronika/CPD/change_point_detection/")
 source("/home/veronika/CPD/change_point_detection/testing/perm_dist_SLURM.R")
-
 fx=sample_mk_function
 alpha_local=0.05
 alpha_global=0.05
@@ -17,14 +16,26 @@ nperm = 100
 
 data_lai <- readRDS("/home/veronika/CPD/data/NOAA_LAI/yearly_median/masked/int10000_NOAA_LAI_masked_median_1981_2020.rds")
 # does it matter for CUSUM if values are multiplied by 10000?
-
+# print(object.size(data_lai),units="auto")
+# library(Matrix)
+# data_lai_sparse<- Matrix(data_lai, sparse = TRUE)
+# print(object.size(data_lai_sparse),units="auto")
+#
 
 res <- perm_dist_SLURM(data=data_lai, fx=fx, nperm=nperm, alpha_local=alpha_local,
                        alpha_global=alpha_global, null_distribution=null_distribution,
                        seed=NULL, block_size=NULL, verbose=TRUE)
+
 filename <- paste0("/home/veronika/CPD/results/NOAA_LAI_tippet_nperm_", nperm, ".rds")
 saveRDS(res, file = filename)
 
+################################ 2d approach ####################################
+data_lai_2d<- array_to_matrix(data_lai)
+
+res <- perm_dist_SLURM_2d(data=data_lai_2d, fx=fx, nperm=nperm, alpha_local=alpha_local,
+                       alpha_global=alpha_global, null_distribution=null_distribution,
+                       seed=NULL, block_size=NULL, verbose=TRUE)
+################################ 2d approach ####################################
 
 
 #**************************************
